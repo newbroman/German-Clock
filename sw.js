ec314b4cabc83d748649fd234f2c4f7d19d48adf
newbroman <105666230+newbroman@users.noreply.github.com>
2026-01-15 21:48:58 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'german-time-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './help_en.html',
+    './help_de.html'
+];
+
+// 1. Install Event: Cache all the assets
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Caching German Time Assets');
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+// 2. Activate Event: Clean up old caches
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+// 3. Fetch Event: Serve from cache, fallback to network
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
